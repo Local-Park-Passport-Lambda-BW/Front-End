344cd5a3f3f87d73127de792ac733ea61b39e3be
Evans Ibok <evansibok@gmail.com>
2019-11-18 15:34:05 +0100

    gotten the nav dropdown to work properly

--- local-park-passport/src/components/DashboardNav.js
+++ local-park-passport/src/components/DashboardNav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import { Navbar, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Navbar, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Button } from 'reactstrap';
 
 import brandLogo from '../images/LP-logo.png'
 import evansImage from '../images/evans.jpg'
@@ -9,21 +9,28 @@ const DashboardNav = () => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <Nav className="ml-auto" navbar>
+        <Nav className="ml-auto navCon" navbar>
           <NavItem>
-            <NavLink href="/components/">Inactive Link</NavLink>
+            {/* <NavLink href="/components/">Inactive Link</NavLink> */}
+          <Button color="success" size="sm" className="addParkButton">Add A Park</Button>{' '}
           </NavItem>
+
           <UncontrolledDropdown setActiveFromChild>
             <DropdownToggle tag="a" className="nav-link" caret>
-              Dropdown
-          </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem tag="a" href="/blah" active>Link</DropdownItem>
+              <span className="charles">Charles Vane</span>
+              <img src={evansImage} alt="profile" className="evansDrop" />
+            </DropdownToggle>
+            <DropdownMenu className="dropMenu">
+              <DropdownItem tag="a" href="/blah" active>View Profile</DropdownItem>
+              <DropdownItem tag="a" href="/blah" active>Parks Visited</DropdownItem>
+              <DropdownItem tag="a" href="/blah" active>Parks Added</DropdownItem>
+              <DropdownItem tag="a" href="/blah" active>Account Settings</DropdownItem>
+              <DropdownItem tag="a" href="/blah" active>Logout</DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
         </Nav>
       </Navbar>
-      
+
     </div>
   )
 }

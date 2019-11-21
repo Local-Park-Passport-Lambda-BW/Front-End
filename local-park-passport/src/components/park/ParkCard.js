d1105ee78006980fd138fa2fda11783193a6f6f2
Alison Law <alisparklaw@gmail.com>
2019-11-21 13:43:48 +0000

    authorization and delete functionality

--- local-park-passport/src/components/park/ParkCard.js
+++ local-park-passport/src/components/park/ParkCard.js
@@ -1,26 +1,27 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
-import parkImage from '../../images/bridge.jpg'
-import Rating from 'react-rating'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import parkImage from "../../images/bridge.jpg";
+import Rating from "react-rating";
+import axios from "axios";
+import getToken from "../GetToken";
 
 // Styles
 const CardCon = styled.div`
   display: flex;
   width: 400px;
-  border: 1px solid #E2E2E2;
+  border: 1px solid #e2e2e2;
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.1);
   border-radius: 0.6em;
   padding: 0.6em;
   margin: 1em;
   background: white;
 
-    img {
-      max-width: 100%;
-      height: auto;
-      align-self: center;
-      border-radius: 0.2em;
-    }
+  img {
+    max-width: 100%;
+    height: auto;
+    align-self: center;
+    border-radius: 0.2em;
+  }
 
   .card-right-con {
     display: flex;
@@ -47,35 +48,50 @@ const CardCon = styled.div`
     }
 
     .ratingCon {
-      
     }
   }
-
 `;
 
-const ParkCard = ({ park }) => {
-
+const ParkCard = props => {
+  const { park, history } = props;
   const id = park.id;
 
   const handleClick = (rating, id) => {
-    console.log(rating, id)
+    console.log(rating, id);
 
-    axios.post(`http://localhost:3300/parks/${id}/ratings/demo`, {
-      rating, comment: "", user_id: 1
-    })
+    axios
+      .post(`https://park-pp.herokuapp.com/parks/${id}/ratings/demo`, {
+        rating,
+        comment: "",
+        user_id: 1
+      })
       .then(res => res.data)
-      .catch(err => err.message)
-  }
+      .catch(err => err.message);
+  };
 
-  useEffect(() => {
+  // console.log(park);
+  // const newId = parkList.id;
+  useEffect(() => {}, [park.rating]);
 
-  }, [park.rating])
-
-
+  // useEffect(() => {
+  // }, []);
+  console.log(props);
+  const deletePark = (event, id) => {
+    console.log(event);
+    event.preventDefault();
+    getToken()
+      .delete(`https://park-pp.herokuapp.com/parks/${id}`)
+      .then(response => history.location.reload(true))
+      .catch(error => console.log(error.message));
+  };
 
   return (
     <CardCon className="cardCon">
-      <img src={parkImage} alt="bridge-park" style={{ width: "120px", height: "120px" }} />
+      <img
+        src={parkImage}
+        alt="bridge-park"
+        style={{ width: "120px", height: "120px" }}
+      />
 
       <div className="card-right-con">
         <h5 className="card-title">{park.name}</h5>
@@ -92,9 +108,17 @@ const ParkCard = ({ park }) => {
           />
           {park.average_rating}
         </div>
+        <div>
+          <button
+            onClick={event => deletePark(event, park.id)}
+            className="parks-button"
+          >
+            Delete park
+          </button>{" "}
+        </div>
       </div>
     </CardCon>
-  )
-}
+  );
+};
 
-export default ParkCard
+export default ParkCard;
